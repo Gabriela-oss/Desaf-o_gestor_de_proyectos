09e9ce6d12422ca93f7c9cc7f6bbffa03d016351
Gabriela Solìs <solisch.gabriela@gmail.com>
2021-03-04 16:58:05 -0600

    segundo git

---
+++ db/migrate/20210304171805_create_projects.rb
@@ -0,0 +1,14 @@
+class CreateProjects < ActiveRecord::Migration[6.1]
+  def change
+    create_table :projects do |t|
+      t.string :name, null: false 
+      t.string :description, null: false
+      t.string :text
+      t.date :started_at
+      t.date :ended_at
+      t.integer :status, default: 0, null: false
+
+      t.timestamps
+    end
+  end
+end
